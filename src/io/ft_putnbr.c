5a468eeb076f9b3b4c15ae2fcb21a82f17f2aba6
Cyril Battistolo <cybattis@z1r5p1.42lyon.fr>
2021-11-24 16:54:03 +0100

    ft_putnbr

--- src/io/ft_putnbr.c
+++ src/io/ft_putnbr.c
@@ -6,13 +6,25 @@
 /*   By: cybattis <cybattis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/08 12:52:34 by cbattist          #+#    #+#             */
-/*   Updated: 2021/11/13 13:23:50 by cybattis         ###   ########.fr       */
+/*   Updated: 2021/11/23 10:17:59 by cybattis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr(int nb)
+void	ft_putnbr(int n)
 {
-	ft_putnbr_fd(nb, 1);
+	char	c;
+	long	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+		ft_putnbr(nb / 10);
+	c = nb % 10 + '0';
+	ft_putchar(c);
 }
